73021cef5dd0b4005f1567e2ddb1aedfdc3e9147
Alex <alex_mcgarry@hotmail.co.uk>
2025-08-11 21:08:43 +0100

    initial commit

---
+++ app/(default)/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/ui/footer';
+import Cursor from '@/components/ui/cursor';
+import AOSInit from '@/components/aos-init';
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AOSInit />
+      <main className="relative flex grow flex-col">{children}</main>
+      <Cursor className="hidden dark:lg:block" />
+
+      <Footer />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        torch:
+          'radial-gradient(525px at 50% 50%, var(--color-accent-torch), transparent 80%)',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
